3c5ba9290d7b9469ed040b549fabfcb08e8601f0
Aditi Ghodke <97653980+Aditi-Ghodke@users.noreply.github.com>
2025-08-03 10:33:22 +0530

    Update UserDao.java

--- PGVerse/src/main/java/com/pgverse/dao/UserDao.java
+++ PGVerse/src/main/java/com/pgverse/dao/UserDao.java
@@ -1,9 +1,9 @@
 package com.pgverse.dao;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pgverse.entities.User;
-import java.util.List;
-import java.util.Optional;
 
 
 public interface UserDao extends JpaRepository<User,Long> {
@@ -13,4 +13,5 @@ public interface UserDao extends JpaRepository<User,Long> {
 	Optional<User> findByEmail(String email);
 	
 	Optional<User> findByUserId(Long userId);
+	
 }
