d2cb8bc1180cfbf7e2f23e79c33e6d8f2aebf2da
Aditi Ghodke <97653980+Aditi-Ghodke@users.noreply.github.com>
2025-08-03 10:34:48 +0530

    Update PgPropertyService.java

--- PGVerse/src/main/java/com/pgverse/service/PgPropertyService.java
+++ PGVerse/src/main/java/com/pgverse/service/PgPropertyService.java
@@ -2,10 +2,18 @@ package com.pgverse.service;
 
 import java.util.List;
 
+import com.pgverse.dto.PGRoomResponseDTO;
 import com.pgverse.dto.PgPropertyRespDTO;
+import com.pgverse.dto.ReviewRespDTO;
 
 public interface PgPropertyService {
 
 	List<PgPropertyRespDTO> getAllPg();
+
+	PgPropertyRespDTO getPgById(Long pgId);
+
+	PGRoomResponseDTO getRoomsPgId(Long pgId);
+
+	List<ReviewRespDTO>  getReviewsByPgId(Long pgId);
 
 }
