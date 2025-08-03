4131ceb9da4deb6d03a3bc8cda4ba87c08cb24b7
Aditi Ghodke <97653980+Aditi-Ghodke@users.noreply.github.com>
2025-08-03 10:32:35 +0530

    Update PgPropertyController.java

--- PGVerse/src/main/java/com/pgverse/controller/PgPropertyController.java
+++ PGVerse/src/main/java/com/pgverse/controller/PgPropertyController.java
@@ -2,8 +2,8 @@ package com.pgverse.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,6 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/pgproperty")
 @AllArgsConstructor
-@Validated
 public class PgPropertyController {
 
 	public final PgPropertyService pgservice;
@@ -23,5 +22,28 @@ public class PgPropertyController {
 	public ResponseEntity<?> getAllPg(){
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(pgservice.getAllPg());
+	}
+	
+	@GetMapping("/{pgId}")
+	public ResponseEntity<?> getPgById(@PathVariable Long pgId)
+	{
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(pgservice.getPgById(pgId));
+	}
+	
+	//GET ALL ROOMS BY PGID
+	@GetMapping("/rooms/{pgId}")
+	public ResponseEntity<?> getRoomsPgId(@PathVariable Long pgId)
+	{
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(pgservice.getRoomsPgId(pgId));
+	}
+	
+	//GET ALL REVIEWS BY PG ID
+	@GetMapping("/reviews/{pgId}")
+	public ResponseEntity<?> getReviewsByPgId(@PathVariable Long pgId)
+	{
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(pgservice.getReviewsByPgId(pgId));
 	}
 }
