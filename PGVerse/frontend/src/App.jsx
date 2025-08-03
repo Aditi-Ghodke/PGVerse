c6ebe6fd327e53766a7aa593a0ecb2ad24a75d49
Aditi Ghodke <97653980+Aditi-Ghodke@users.noreply.github.com>
2025-08-03 10:30:04 +0530

    Update App.jsx

--- PGVerse/frontend/src/App.jsx
+++ PGVerse/frontend/src/App.jsx
@@ -1,6 +1,83 @@
- import { BrowserRouter, Routes, Route } from "react-router-dom";
+//  import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './App.css'
+// import Login from './pages/Login'
+// import UserLayout from "./layouts/UserLayout";
+// import OwnerLayout from "./layouts/OwnerLayout";
+// import AdminLayout from "./layouts/AdminLayout";
+
+// import UserDashboard from "./pages/user/UserDashboard";
+// import OwnerDashboard from "./pages/owner/OwnerDashboard";
+// import AdminDashboard from "./pages/admin/AdminDashboard";
+
+// import NotFound from "./pages/NotFound";
+// import PrivateRoute from "./routes/PrivateRoute";
+
+
+
+// function App() {
+//   return (
+//     <>
+//      <BrowserRouter>
+//      <Routes>
+//       <Route path='/' element={<Login/>}></Route>
+
+//        {/* User Routes */}
+//         <Route
+//           path="/user/*"
+//           element={
+//             <PrivateRoute allowedRole="USER">
+//               <UserLayout />
+//             </PrivateRoute>
+//           }
+//         >
+//           <Route path="dashboard" element={<UserDashboard />} />
+//         </Route>
+
+//         {/* Owner Routes */}
+//         <Route
+//           path="/owner/*"
+//           element={
+//             <PrivateRoute allowedRole="OWNER">
+//              <OwnerLayout/>
+//             </PrivateRoute>
+//           }
+//         >
+//           <Route path="dashboard" element={<OwnerDashboard />} />
+//         </Route>
+
+//         {/* Admin Routes */}
+//         <Route
+//           path="/admin/*"
+//           element={
+//             <PrivateRoute allowedRole="ADMIN">
+//               <AdminLayout />
+//             </PrivateRoute>
+//           }
+//         >
+//           <Route path="dashboard" element={<AdminDashboard />} />
+//         </Route>
+
+//         {/* Fallback Route */}
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+     
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+
 import Login from './pages/Login'
+import LandingPage from "../src/pages/LandingPage";
+import HomePage from '../src/pages/Home'
+import PgDetailsPage from '../src/pages/PgDetailsPage'
+
 import UserLayout from "./layouts/UserLayout";
 import OwnerLayout from "./layouts/OwnerLayout";
 import AdminLayout from "./layouts/AdminLayout";
@@ -12,59 +89,68 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./routes/PrivateRoute";
 
-
-
 function App() {
   return (
-    <>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Login/>}></Route>
-      {/* <Route path='/register' element={<Register/>}></Route> */}
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <BrowserRouter>
+        <Routes>
 
-       {/* User Routes */}
-        <Route
-          path="/user/*"
-          element={
-            <PrivateRoute allowedRole="USER">
-              <UserLayout />
-            </PrivateRoute>
-          }
-        >
-          <Route path="dashboard" element={<UserDashboard />} />
-        </Route>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/pg/:pgId" element={<PgDetailsPage />} />
+          <Route
+            path="/login"
+            element={
+              <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-indigo-600">
+                <Login />
+              </div>
+            }
+          />
 
-        {/* Owner Routes */}
-        <Route
-          path="/owner/*"
-          element={
-            <PrivateRoute allowedRole="OWNER">
-             <OwnerLayout/>
-            </PrivateRoute>
-          }
-        >
-          <Route path="dashboard" element={<OwnerDashboard />} />
-        </Route>
+          {/* User Routes */}
+          <Route
+            path="/user/*"
+            element={
+              <PrivateRoute allowedRole="USER">
+                <UserLayout />
+              </PrivateRoute>
+            }
+          >
+            <Route path="dashboard" element={<UserDashboard />} />
+          </Route>
 
-        {/* Admin Routes */}
-        <Route
-          path="/admin/*"
-          element={
-            <PrivateRoute allowedRole="ADMIN">
-              <AdminLayout />
-            </PrivateRoute>
-          }
-        >
-          <Route path="dashboard" element={<AdminDashboard />} />
-        </Route>
+          {/* Owner Routes */}
+          <Route
+            path="/owner/*"
+            element={
+              <PrivateRoute allowedRole="OWNER">
+                <OwnerLayout />
+              </PrivateRoute>
+            }
+          >
+            <Route path="dashboard" element={<OwnerDashboard />} />
+          </Route>
 
-        {/* Fallback Route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-     
-     </BrowserRouter>
-    </>
-  )
+          {/* Admin Routes */}
+          <Route
+            path="/admin/*"
+            element={
+              <PrivateRoute allowedRole="ADMIN">
+                <AdminLayout />
+              </PrivateRoute>
+            }
+          >
+            <Route path="dashboard" element={<AdminDashboard />} />
+          </Route>
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
