43c52e585f17389e1239c1e5eb03a38a1caef350
Aditi Ghodke <97653980+Aditi-Ghodke@users.noreply.github.com>
2025-08-07 09:12:58 +0530

    Update adminApi.js

--- PGVerse/frontend/src/api/adminApi.js
+++ PGVerse/frontend/src/api/adminApi.js
@@ -51,3 +51,41 @@ export const deleteOwnerById = async (id, token) => {
   );
   return res.data;
 };
+
+//----------REVIEWS-----------
+
+// Get reviews by PG ID
+export const getReviewsByPgId = async (pgId, token) => {
+  const res = await axios.get(
+    `${BASE_URL}/pgproperty/${pgId}/reviews`,
+    authHeaders(token)
+  );
+  return res.data;
+};
+
+// Get all PG properties
+export const getAllPgProperties = async (token) => {
+  const res = await axios.get(`${BASE_URL}/pgproperty`, authHeaders(token));
+  return res.data;
+};
+
+
+// ----------BOOKINGS-----------
+
+// Get all bookings for a specific PG by PG ID
+export const getBookingsByPgId = async (pgId, token) => {
+  const res = await axios.get(
+    `${BASE_URL}/bookings/pgproperty/${pgId}`,
+    authHeaders(token)
+  );
+  return res.data;
+};
+
+// Get a specific booking by Booking ID
+export const getBookingById = async (bookingId, token) => {
+  const res = await axios.get(
+    `${BASE_URL}/bookings/${bookingId}`,
+    authHeaders(token)
+  );
+  return res.data;
+};
